6b9f1f13b8034aee6c74189972192f7e852a5bc2
MewKay <125735249+MewKay@users.noreply.github.com>
2023-08-31 18:43:24 +0300

    Create function to display knight's moves

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import KnightsGraph from "./KnightsGraph";
 
-KnightsGraph.print();
+const knightMoves = function displayEachMoveKnightHasToPlay(
+  startingCoordinates,
+  destinationCoordinates
+) {
+  let knightsPath = KnightsGraph.shortPathbfs(
+    startingCoordinates,
+    destinationCoordinates
+  );
+
+  console.log(
+    `=> You made it in ${knightsPath.length - 1} moves! Here's your path:`
+  );
+  knightsPath.forEach((move) => console.log(move));
+};
+
+knightMoves([3, 3], [4, 3]);
